1cac22cb5f4b51b884caff97f739eecb721728bc
ArthurWyon <arthur.wyon@numericable.fr>
2024-10-11 17:29:21 +0200

    :sparkles:/ correction du constructor

--- src/entities/character.entity.ts
+++ src/entities/character.entity.ts
@@ -1,3 +1,11 @@
+import { Race } from "./race.entity";
+import { Alignment } from "./alignment.entity";
+import { Attributes } from "./attributes.entity";
+import { Skills } from "./skills.entity";
+import { Equipment } from "./equipment.entity";
+import { Skill } from "./skill.entity";
+import { Attribute } from "./attribute.entity";
+
 export class character {
   private _name: string;
   private _image: string;
@@ -10,14 +18,38 @@ export class character {
   constructor(
     name: string,
     image: string,
+    race: Race,
+    alignment: Alignment,
+    attributes: Attributes,
+    skills: Skills,
+    equipment: Equipment,
   ) {
     this._name = name;
     this._image = image;
-    
-    this._race = new Race(); 
-    this._alignment = new Alignment();
-    this._attributes = new Attributes(); 
-    this._skills = new Skills();
-    this._equipment = new Equipment(); 
+    this._race = Race();
+    this._alignment = Alignment();
+    this._attributes = Attributes();
+    this._skills = Skills();
+    this._equipment = Equipment();
   }
-  
+
+  get name(): string {
+    return this._name;
+  }
+
+  get image(): string {
+    return this._image;
+  }
+
+  addskill(skill: Skill): void {
+    this._skills.add(skill);
+  }
+
+  modifyattribute(attribute: Attribute): void {
+    this._attributes.modify(attribute);
+  }
+
+  saveToUserAccount(): void {}
+
+  getPDF(): void {}
+}
