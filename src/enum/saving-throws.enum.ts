77a9fd2ec9083191ea35bf9d7fdd359226684b4b
Liès <lies.tiguercha@outlook.com>
2024-10-11 17:32:19 +0200

    :sparkles: (feat): Add enum for Saving Throws

---
+++ src/enum/saving-throws.enum.ts
@@ -0,0 +1,8 @@
+export enum SavingThrows {
+  STRENGTH = "Strength",
+  DEXTERITY = "Dexterity",
+  CONSTITUTION = "Constitution",
+  INTELLIGENCE = "Intelligence",
+  WISDOM = "Wisdom",
+  CHARISMA = "Charisma",
+}
