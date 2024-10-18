57515083c36e5ca1f119ef9a2ab902d037c24667
KendoSensei <erwan.beuselinck@gmail.com>
2024-10-18 17:28:32 +0200

    :sparkles: (feat) add createCharacterUseCase logic

---
+++ src/usecase/CreateCharacterUseCase.ts
@@ -0,0 +1,19 @@
+import { error } from "console";
+import { Character } from "entities/character.entity";
+import { CharacterService } from "services/CharacterService";
+
+export class CreateCharacterUseCase {
+  private characterService: CharacterService;
+
+  constructor(characterService: CharacterService) {
+    this.characterService = characterService;
+  }
+
+  async execute(character: Character) {
+    try {
+      this.characterService.createCharacter(character);
+    } catch (e: unknown) {
+      throw new Error("respect les types parce que la t'es ma cuti");
+    }
+  }
+}
--- src/usecase/FindCharacterUseCase.ts
+++
@@ -1,7 +0,0 @@
-import { UseCase } from "./UseCase";
-
-export class FindCharacterUseCase implements UseCase {
-  execute(): void {
-    console.log("Finding character...");
-  }
-}
